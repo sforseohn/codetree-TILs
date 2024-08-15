64926e121e28b9e8fbcf86e9ab63f4352c664640
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-08-15 23:19:02 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240815/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ 240815/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -1,14 +1,19 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
-left = right = 0
+left = 0
 max_range = 1
+seen = set()
 
-while right < n - 1:
-    right += 1
+for right in range(n-1):
+    new = nums[right]
 
-    if nums[right] == nums[left]: # 중복 발생
-        left += 1
+    if not new in seen: # 아직 발견되지 않았으면 추가
+        seen.add(new)
+    else:               # 중복 발생
+        while left <= right and new in seen:
+            seen.remove(nums[left])
+            left += 1
 
     current_range = right - left + 1
     max_range = max(max_range, current_range)
