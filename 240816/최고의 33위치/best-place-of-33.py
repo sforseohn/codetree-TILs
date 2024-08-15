2335e1b62535eeb05bade1515099e793941054cb
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-08-16 00:39:42 +0900

    Title: 최고의 33위치, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/최고의 33위치/best-place-of-33.py
+++ 240816/최고의 33위치/best-place-of-33.py
@@ -6,8 +6,9 @@ for i in range(len(grid) - 3 + 1):
     for j in range(len(grid) - 3 + 1):
         cnt = 0
         for x in range(3):
-            cnt += sum(grid[i + x])
-            
+            for y in range(3):
+                cnt += grid[i + x][j + y]
+
         max_cnt = max(max_cnt, cnt)
 
 print(max_cnt)
