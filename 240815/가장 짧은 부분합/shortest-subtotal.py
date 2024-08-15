d36b83d9ec1a81c2fd69282316a13441cf52f505
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-08-15 21:49:54 +0900

    Title: 가장 짧은 부분합, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240815/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,21 @@
+# 입력
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+
+# 연산
+shortest_range = n + 1
+for i in range(n-1):
+    j = i+1
+    while (j < n):
+        temp_sum = sum(nums[i:j+1])
+        cur_range = j - i + 1
+        if temp_sum >= s and cur_range < shortest_range:
+            shortest_range = cur_range
+            break
+        j += 1
+
+# 출력
+if shortest_range == n+1:
+    print(-1) 
+else:
+    print(shortest_range)
