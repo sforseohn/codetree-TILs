8369b414e8295bca112392c00fa3c591b93b9a14
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-08-15 21:57:52 +0900

    Title: 가장 짧은 부분합, Time: 5068ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240815/가장 짧은 부분합/shortest-subtotal.py
+++ 240815/가장 짧은 부분합/shortest-subtotal.py
@@ -5,18 +5,16 @@ nums = list(map(int, input().split()))
 # 연산
 shortest_range = n + 1
 for i in range(n-1):
-    j = i
-    while (j < n):
+    for j in range(i, n):
         temp_sum = sum(nums[i:j+1])
         cur_range = j - i + 1
 
         if cur_range >= shortest_range:
             break
 
-        if temp_sum >= s and cur_range < shortest_range:
-            shortest_range = cur_range
+        if temp_sum >= s:
+            shortest_range = min(shortest_range, cur_range)
             break
-        j += 1
 
 # 출력
 if shortest_range == n+1:
