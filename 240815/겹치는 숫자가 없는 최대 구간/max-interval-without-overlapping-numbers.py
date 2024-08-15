67479427c17410ca66ce6522e403bde3ddd5374d
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-08-15 23:21:12 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 156ms, Memory: 34MB, Status: Passed - CodeTree

--- 240815/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ 240815/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -5,13 +5,13 @@ left = 0
 max_range = 1
 seen = set()
 
-for right in range(n-1):
+for right in range(n):
     new = nums[right]
 
     while left <= right and new in seen:
         seen.remove(nums[left])
         left += 1
-        
+
     seen.add(new)
 
     current_range = right - left + 1
